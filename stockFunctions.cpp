af46d97baecb19b7a6f1850a44d09e08061c54ad
A Student <student@nowhere.edu>
2026-03-08 01:38:50 +0000

    Mar 7, 2026, 5:38 PM

---
+++ stockFunctions.cpp
@@ -0,0 +1,11 @@
+#include "stockFunctions.h"
+#include <vector>
+#include <string>
+
+double percentChange(const std::vector<double>& values , int startDay , int endDay) {
+  return 0.0;
+}
+
+double highestValue(const std::vector<double>& values , int startDay , int endDay) {
+  return 0.0;
+}
