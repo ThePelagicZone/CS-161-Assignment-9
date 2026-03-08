83645ab263523df4b7005c588b7d503eb50d6684
A Student <student@nowhere.edu>
2026-03-08 01:42:52 +0000

    Mar 7, 2026, 5:42 PM

--- stockFunctions.cpp
+++ stockFunctions.cpp
@@ -18,3 +18,12 @@ double standardDev(const std::vector<double>& values , int startDay , int endDay
   return 0.0;
 }
 
+std::vector<double> parseData(const std::string& csv) {
+  return std::vector<double>();
+}
+
+std::vector<double> getChangeVector(const std::vector<double>& values) {
+  return std::vector<double>();
+}
+
+double maxDrawdown(const std::vector<double>)
