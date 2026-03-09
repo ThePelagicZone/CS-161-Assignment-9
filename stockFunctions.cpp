ed1bc49daa59faf9a2877af6980b471800687078
A Student <student@nowhere.edu>
2026-03-09 23:25:29 +0000

    Mar 9, 2026, 4:25 PM

--- stockFunctions.cpp
+++ stockFunctions.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <string>
 #include <cmath>
-#
+#include <algorithm>      // I was looking at libraries that streamlined potentially long for loops and found one that might work for this assignment
 
 double percentChange(const std::vector<double>& values , int startDay , int endDay) {
   return 0.0;
