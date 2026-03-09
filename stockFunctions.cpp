c6b7011526c9c897b8c316245bda324bd82942b6
A Student <student@nowhere.edu>
2026-03-09 23:26:10 +0000

    Mar 9, 2026, 4:26 PM

--- stockFunctions.cpp
+++ stockFunctions.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>      // I was looking at libraries that streamlined potentially long for loops and found one that might work for this assignment
 
 double percentChange(const std::vector<double>& values , int startDay , int endDay) {
-  return 0.0;
+  return (values.at(endDay) - values.at(startDay))
 }
 
 double highestValue(const std::vector<double>& values , int startDay , int endDay) {
