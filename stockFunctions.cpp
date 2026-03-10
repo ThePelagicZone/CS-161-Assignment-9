9e9e4803f1b9bb169d24925134c9831149db5b90
A Student <student@nowhere.edu>
2026-03-10 03:33:34 +0000

    Mar 9, 2026, 8:33 PM

--- stockFunctions.cpp
+++ stockFunctions.cpp
@@ -1,17 +1,20 @@
 #include "stockFunctions.h"
-#include <vector>
-#include <string>
 #include <cmath>
-#include <algorithm>      // I was looking at libraries that streamlined potentially long for loops and found one that might work for this assignment
+#include <string>
+#include <vector>
 
-double percentChange(const std::vector<double>& values , int startDay , int endDay) {
-  return ((values.at(endDay) - values.at(startDay)) / values.at(startDay)) * 100;
+double percentChange(const std::vector<double>& values, int startDay,
+                     int endDay)
+{
+  return ((values.at(endDay) - values.at(startDay)) / values.at(startDay))
+         * 100;
 }
 
-double highestValue(const std::vector<double>& values , int startDay , int endDay) {
+double highestValue(const std::vector<double>& values, int startDay, int endDay)
+{
   double maxSoFar = values.at(startDay);
 
-  for (int i = startDay + 1 ; i <= endDay ; i++) {
+  for (int i = startDay + 1; i <= endDay; i++) {
     if (values.at(i) > maxSoFar) {
       maxSoFar = values.at(i);
     }
@@ -19,33 +22,36 @@ double highestValue(const std::vector<double>& values , int startDay , int endDa
   return maxSoFar;
 }
 
-double average(const std::vector<double>& values , int startDay , int endDay) {
+double average(const std::vector<double>& values, int startDay, int endDay)
+{
   // Parameters
   double sum = 0;
 
   // Main Loop
   int count = endDay - startDay + 1;
-  for (int i = startDay ; i <= endDay ; i++) {
+  for (int i = startDay; i <= endDay; i++) {
     sum += values.at(i);
   }
   // Sum / # of observations
   return sum / count;
 }
 
-double standardDev(const std::vector<double>& values , int startDay , int endDay) {
+double standardDev(const std::vector<double>& values, int startDay, int endDay)
+{
   // Parameters
-  double avg = average(values , startDay , endDay);
+  double avg = average(values, startDay, endDay);
   double sumSquareDiff = 0;
 
-  //Main Loop
-  for (int i = startDay ; i <= endDay ; i++) {
-    sumSquareDiff += std::pow(values.at(i) - avg , 2);
+  // Main Loop
+  for (int i = startDay; i <= endDay; i++) {
+    sumSquareDiff += std::pow(values.at(i) - avg, 2);
   }
-  //Divide by variance - then sqrt
+  // Divide by variance - then sqrt
   return std::sqrt(sumSquareDiff / (endDay - startDay + 1));
 }
 
-std::vector<double> parseData(const std::string& csv) {
+std::vector<double> parseData(const std::string& csv)
+{
   // Parameters
   std::vector<double> result;
 
@@ -54,16 +60,17 @@ std::vector<double> parseData(const std::string& csv) {
 
   // Main Loop
   while (end != std::string::npos) {
-    result.push_back(std::stod(csv.substr(start , end - start)));
+    result.push_back(std::stod(csv.substr(start, end - start)));
     start = end + 1;
-    end = csv.find (',' , start);
+    end = csv.find(',', start);
   }
   // Process after final comma
   result.push_back(std::stod(csv.substr(start)));
   return result;
 }
 
-std::vector<double> getChangeVector(const std::vector<double>& values) {
+std::vector<double> getChangeVector(const std::vector<double>& values)
+{
   // Parameters
   std::vector<double> changes(values.size());
 
@@ -72,19 +79,20 @@ std::vector<double> getChangeVector(const std::vector<double>& values) {
     return changes;
   }
   // Main Loop
-  for (size_t i = 1 ; i < values.size() ; i++) {
-    changes.at(i) = values.at(i) - values.at(i-1);
+  for (size_t i = 1; i < values.size(); i++) {
+    changes.at(i) = values.at(i) - values.at(i - 1);
   }
   return changes;
 }
 
-double maxDrawdown(const std::vector<double>& values , int startDay , int endDay) {
+double maxDrawdown(const std::vector<double>& values, int startDay, int endDay)
+{
   // Parameters
   double minDiff = 0;
 
-  //Main Loop
-  for (int i = startDay ; i <= endDay ; i++) {
-    for (int j = i ; j <= endDay ; j++) {
+  // Main Loop
+  for (int i = startDay; i <= endDay; i++) {
+    for (int j = i; j <= endDay; j++) {
       double diff = values.at(j) - values.at(i);
       if (diff < minDiff) {
         minDiff = diff;
